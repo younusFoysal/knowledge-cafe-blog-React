7d8b701dcd2d064d84580e8617a36e093497fb01
younusFoysal <younus.faysal@gmail.com>
2024-04-14 12:21:59 +0600

    OK

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+import Header from "./components/Header/Header.jsx";
+import Blogs from "./components/Blogs/Blogs.jsx";
+
+function App() {
+
+
+  return (
+      <>
+          <Header></Header>
+          <Blogs></Blogs>
+      </>
+  )
+}
+
+export default App
---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,21 @@
+import {useEffect, useState} from "react";
+
+
+const Blogs = () => {
+
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        fetch('blogs.JSON')
+            .then(res => res.json())
+            .then(data => setBlogs(data))
+    }, []);
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Blogs;
